bb90611b5f93cc4e778a6d7694590f67e36b92ab
aljorgevi <jormencar@yahoo.com>
2022-04-15 01:19:29 +0100

    add logger & width & heigh from img

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
 import fs from 'fs-extra'
 import axios from 'axios'
+import { getImageSize } from './getImageSize.js'
+
+const logger = (...args) => console.log('[🤤 dirty-scrapper]', ...args)
 
 const { writeJSON } = fs
 
-const INITIAL_ID_XKCD_COMIC = 2500
-const MAX_ID_XKCD_COMIC = 2505
+const INITIAL_ID_XKCD_COMIC = 2587
+const MAX_ID_XKCD_COMIC = 2588
 
 for (let id = INITIAL_ID_XKCD_COMIC; id < MAX_ID_XKCD_COMIC; id++) {
   const url = `https://xkcd.com/${id}/info.0.json`
+  logger(`Fetching ${url}...`)
 
   const { data } = await axios.get(url)
-  const { num, news, transcript, ...resOfComic } = data
+  const { num, news, transcript, img, ...resOfComic } = data
+  logger(`Fetched comic #${num}. Getting image dimensions...`)
+  const { height, width } = await getImageSize({ url: img })
+  logger(`Got image dimensions: ${width}x${height}`)
 
   const comicToStore = {
     id,
+    height,
+    width,
     ...resOfComic,
   }
 
-  await writeJSON(`./comics/${id}.json`, comicToStore)
-  console.log(data)
+  const jsonFile = `./comics/${id}.json`
+  await writeJSON(jsonFile, comicToStore)
+  logger(`Wrote ${jsonFile}! 🎉`)
 }
